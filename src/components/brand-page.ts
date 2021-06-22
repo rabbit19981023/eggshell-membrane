5e74250dc2b4361bd3ee2ef09d9e53a5aa5f5681
rabbit19981023 <rabbit19981023@gmail.com>
2021-06-22 14:54:12 +0800

    test

--- src/components/brand-page.ts
+++ src/components/brand-page.ts
@@ -2,11 +2,11 @@ const BrandPage: string = `
   <div id="brand">
   <h1 class="brand-title fw-bold"></h1>
 
-  <img alt="brand-img-1" class="brand-img brand-img-1">
+  <img class="brand-img brand-img-1">
 
   <div class="brand-content"></div>
 
-  <img alt="brand-img-2" class="brand-img brand-img-2">
+  <img class="brand-img brand-img-2">
   </div>
 `
 
--- src/components/secret.ts
+++ src/components/secret.ts
@@ -1,6 +1,6 @@
 const Secret: string = `
   <div class="eggshell-secret">
-    <img alt="eggshell-secret" class="eggshell-secret-img">
+    <img class="eggshell-secret-img">
 
     <div class="eggshell-secret-intro mt-5 mt-lg-0">
       <h1 class="eggshell-secret-title"></h1>
